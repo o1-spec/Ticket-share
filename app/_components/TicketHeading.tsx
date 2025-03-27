c1a9fb391da2f7097b59a31588f8a58496f339ca
o1-spec <oluwafemionadokun@gmail.com>
2025-03-28 00:08:52 +0100

    feat:added responsiveness

--- app/_components/TicketHeading.tsx
+++ app/_components/TicketHeading.tsx
@@ -1,10 +1,13 @@
 import { X } from "lucide-react";
+import Link from "next/link";
 
 function TicketHeading() {
   return (
     <div className="flex justify-between items-center bg-[#212530] w-full p-3">
       <div>
-        <X size={24} color="white" />
+        <Link href="/register">
+          <X size={24} color="white" />
+        </Link>
       </div>
       <div>
         <span className="text-white text-[17px]">My Tickets</span>
